b9a56e3025d01f984a2b1da404cb7a5c357df9bb
MaraOD555 <ruffbc@hotmail.com>
2022-10-04 12:32:51 +0300

    базовый коммит

---
+++ src/main/java/ru/hogwarts/school/HW3_database/Hw3DatabaseApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school.HW3_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw3DatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw3DatabaseApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/hogwarts/school/HW3_database/service/FacultyService.java
@@ -0,0 +1,41 @@
+package ru.hogwarts.school.HW3_database.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.HW3_database.model.Faculty;
+
+import java.util.*;
+
+@Service
+public class FacultyService {
+    private final Map<Long, Faculty> faculties = new HashMap<>();
+    private long count = 0;
+
+    public Faculty createFaculty(Faculty faculty) {
+        faculty.setId(count++);
+        faculties.put(faculty.getId(), faculty);
+        return faculty;
+    }
+    public Faculty findFaculty(long Id) {
+        return faculties.get(Id);
+    }
+    public Faculty editFaculty(Faculty faculty) {
+        if (faculties.containsKey(faculty.getId())) {
+            faculties.put(faculty.getId(), faculty);
+            return faculty;
+        }
+        return null;
+    }
+
+    public Faculty deleteFaculty(long Id){
+        return faculties.remove(Id);
+    }
+    public Collection<Faculty> findFacultyByColor(String color) {
+        ArrayList<Faculty> listOfFaculties = new ArrayList<>();
+        for (Faculty faculty : faculties.values()) {
+            if (Objects.equals(faculty.getColor(), color)) {
+                listOfFaculties.add(faculty);
+            }
+        }
+        return listOfFaculties;
+    }
+}
